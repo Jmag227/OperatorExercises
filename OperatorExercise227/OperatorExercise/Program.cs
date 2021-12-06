d6a6d23317116b8ee3c0d76961d99b74ee925832
Jarom Magalei <jarom.pro.magalei@gmail.com>
2021-12-06 13:40:18 -0700

    first commit

---
+++ OperatorExercise227/OperatorExercise/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Div();
+            var result = Math();
+            Console.WriteLine(result);
+        }
+
+        public static void Div()
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine("if a = 2, and b = 4...");
+
+            Console.WriteLine($"Then 17/4 is 4 remainder {remainder}");
+
+        }
+
+        public static double AreaOfACircle(double r)
+        {
+            double pi = 3.14;
+            double anwer = pi * r * r;
+            return anwer;        
+
+        }
+
+        public static double Math()
+        {
+            Console.WriteLine("Pick a radius of a circle.");
+            var r = double.Parse(Console.ReadLine());
+            return AreaOfACircle(r);
+
+             
+        }
+    
+    
+    }
+
+
+
+
+}
